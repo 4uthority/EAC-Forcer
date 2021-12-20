624fe47d13a9cc599b1c038d6c1aeafb319e8c3c
4uthority <95937450+4uthority@users.noreply.github.com>
2021-12-20 18:28:56 +0000

    Update forcer.bat

--- forcer.bat
+++ forcer.bat
@@ -5,23 +5,36 @@ title EAC Forcer
 cls
 color 9
 echo.
-
+taskkill -im FortniteClient-Win64-Shipping_EAC.exe
+taskkill -im FortniteClient-Win64-Shipping_BE.exe
+taskkill -im FortniteClient-Win64-Shipping.exe
+cls
+echo.
 echo [Info]: RUN AS ADMIN.
 set /p menu="[Y/N] Force EAC?: "
 if %menu%==Y goto Yes
 if %menu%==y goto Yes
-if %menu%==N goto No
-if %menu%==n goto No
-cls
+
 
 :Yes
-echo Forcing Easy Anti Cheat, please wait.
-set /p pause="[Info]: Wait 3 Seconds & Press enter."
-echo Forced!
-
-date 10-31-2021
-
-:No
+cls
 echo.
-echo Force EAC you fucking retard.
-set /p pause=" "
+echo Forcing Easy Anti Cheat, please wait.
+ping localhost -n 2 >nul
+cls
+echo.
+echo Forcing Easy Anti Cheat, please wait..
+ping localhost -n 2 >nul
+cls
+echo.
+echo Forcing Easy Anti Cheat, please wait...
+ping localhost -n 2 >nul
+cls
+echo.
+goto forced
+cls
+:forced
+echo Forced!
+pause
+date 10-31-2021
+start https://sellix.io/authority
